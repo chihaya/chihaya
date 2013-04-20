09fa4cca5e90b8d2f192fc6fe8eabeeac453382f
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2013-04-19 22:43:55 -0400

    bufferpool docs updated

--- util/bufferpool.go
+++ util/bufferpool.go
@@ -1,19 +1,17 @@
-/*
- * This file is part of Chihaya.
- *
- * Chihaya is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Chihaya is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Chihaya.  If not, see <http://www.gnu.org/licenses/>.
- */
+// This file is part of Chihaya.
+//
+// Chihaya is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Chihaya is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Chihaya.  If not, see <http://www.gnu.org/licenses/>.
 
 package util
 
@@ -21,11 +19,15 @@ import (
 	"bytes"
 )
 
+// BufferPool allows one to easily reuse a limited-sized pool of equally sized
+// buffers.
 type BufferPool struct {
 	bufSize int
 	pool    chan *bytes.Buffer
 }
 
+// NewBufferPool returns a newly allocated BufferPool with the given pool size
+// and buffer size.
 func NewBufferPool(size int, bufSize int) *BufferPool {
 	return &BufferPool{
 		bufSize,
@@ -33,6 +35,8 @@ func NewBufferPool(size int, bufSize int) *BufferPool {
 	}
 }
 
+// Take is used to obtain a new zeroed buffer. This may or may not have been
+// recycled from the pool depending on factors such as pool being empty.
 func (pool *BufferPool) Take() (buf *bytes.Buffer) {
 	select {
 	case buf = <-pool.pool:
@@ -44,6 +48,8 @@ func (pool *BufferPool) Take() (buf *bytes.Buffer) {
 	return
 }
 
+// Give is used to attempt to add a buffer to the pool. This may or may not
+// be added to the pool depending on factors such as the pool being full.
 func (pool *BufferPool) Give(buf *bytes.Buffer) {
 	select {
 	case pool.pool <- buf:
