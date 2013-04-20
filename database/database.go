8768edf0b93f23424f9b7173aa22c7717fe658da
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2013-04-20 02:25:32 -0400

    package descriptions for server and database

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the BSD 2-Clause license,
 // which can be found in the LICENSE file.
 
+// Package database implements all database interactions used by a Chihaya server.
 package database
 
 import (
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the BSD 2-Clause license,
 // which can be found in the LICENSE file.
 
+// Package server implements a torrent tracker.
 package server
 
 import (
