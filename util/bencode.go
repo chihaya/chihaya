b3c16caa36da7e3611ccf55848d4e96c3ed5df6b
kotoko <kotoko@kotoko.jp>
2013-04-20 00:04:02 -0400

    Change license to BSD

--- util/bencode.go
+++ util/bencode.go
@@ -1,19 +1,6 @@
-// This file is part of Chihaya.
-//
-// Chihaya is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Chihaya is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Chihaya.  If not, see <http://www.gnu.org/licenses/>.
-
-package util
+// Copyright 2013 The Chihaya Authors. All rights reserved.
+// Use of this source code is governed by the BSD 2-Clause license,
+// which can be found in the LICENSE file.
 
 import (
 	"bytes"
