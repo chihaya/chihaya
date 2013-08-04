c8c34df277855485dc5ef3c39c85f2cacb6323ad
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2013-08-04 01:26:14 -0400

    vagrant support

---
+++ vagrant/puppet/manifests/default.pp
@@ -0,0 +1,28 @@
+Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/' }
+exec { 'echo this works': }
+
+group { 'puppet': ensure => 'present' }
+
+exec { 'apt-get update':
+	command => '/usr/bin/apt-get update',
+}
+
+exec { 'add-apt-repository ppa:duh/golang && apt-get update':
+	alias   => 'go_repo',
+	creates => '/etc/apt/sources.list.d/gophers-go-precise.list',
+	require => Package['python-software-properties'],
+}
+
+package { 'python-software-properties':
+	ensure  => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'golang':
+	ensure  => present,
+	require => Exec['go_repo'],
+}
+
+exec { 'echo "export GOPATH=/home/vagrant/chihaya" > /etc/profile.d/gopath.sh':
+  creates => '/etc/profile.d/gopath.sh',
+}
