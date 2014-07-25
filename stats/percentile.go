dda56c43572c9be236c42f819ee41fe93c53fc9d
Justin Li <jli.justinli@gmail.com>
2014-07-25 04:23:24 -0400

    Add missing copyright headers

--- stats/percentile.go
+++ stats/percentile.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Chihaya Authors. All rights reserved.
+// Use of this source code is governed by the BSD 2-Clause license,
+// which can be found in the LICENSE file.
+
 package stats
 
 import (
