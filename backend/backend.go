87c68f1973e5441b73d38c87b79216d9431d0f42
Justin Li <jli.justinli@gmail.com>
2015-01-01 12:02:25 -0500

    2015 :sparkles:

--- backend/backend.go
+++ backend/backend.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The Chihaya Authors. All rights reserved.
+// Copyright 2015 The Chihaya Authors. All rights reserved.
 // Use of this source code is governed by the BSD 2-Clause license,
 // which can be found in the LICENSE file.
 
