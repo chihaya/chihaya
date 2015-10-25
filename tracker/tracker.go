b6929a7632e640b5805bac06b726a30337d70534
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2015-10-25 16:31:46 -0400

    remove unused Server type

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -24,15 +24,6 @@ type Tracker struct {
 	*Storage
 }
 
-// Server represents a server for a given BitTorrent tracker protocol.
-type Server interface {
-	// Serve runs the server and blocks until the server has shut down.
-	Serve(addr string)
-
-	// Stop cleanly shuts down the server in a non-blocking manner.
-	Stop()
-}
-
 // New creates a new Tracker, and opens any necessary connections.
 // Maintenance routines are automatically spawned in the background.
 func New(cfg *config.Config) (*Tracker, error) {
