3a1b321528d75681fe5e3b583c7e7193be074f7a
Leo Balduf <balduf@hm.edu>
2016-03-30 15:29:04 -0400

    middleware: fixed client middleware package

--- server/store/middleware/client/whitelist.go
+++ server/store/middleware/client/whitelist.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the BSD 2-Clause license,
 // which can be found in the LICENSE file.
 
-package ip
+package client
 
 import (
 	"github.com/chihaya/chihaya"
