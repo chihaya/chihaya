6e1cfa18d896625a59833e8ddad724c059c3e5ad
Leo Balduf <baldufle@hu-berlin.de>
2017-06-17 20:31:53 +0200

    bittorrent: make invalid query escape errors static

--- bittorrent/params.go
+++ bittorrent/params.go
@@ -5,6 +5,8 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 // Params is used to fetch (optional) request parameters from an Announce.
@@ -38,6 +40,10 @@ var ErrKeyNotFound = errors.New("query: value for the provided key does not exis
 // ErrInvalidInfohash is returned when parsing a query encounters an infohash
 // with invalid length.
 var ErrInvalidInfohash = ClientError("provided invalid infohash")
+
+// ErrInvalidQueryEscape is returned when a query string contains invalid
+// escapes.
+var ErrInvalidQueryEscape = ClientError("invalid query escape")
 
 // QueryParams parses a URL Query and implements the Params interface with some
 // additional helpers.
@@ -113,11 +119,21 @@ func parseQuery(query string) (q *QueryParams, err error) {
 		}
 		key, err = url.QueryUnescape(key)
 		if err != nil {
-			return nil, err
+			// QueryUnescape returns an error like "invalid escape: '%x'".
+			// But frontends record these errors to prometheus, which generates
+			// a lot of time series.
+			// We log it here for debugging instead.
+			log.WithFields(log.Fields{"error": err}).Debug("failed to unescape query param key")
+			return nil, ErrInvalidQueryEscape
 		}
 		value, err = url.QueryUnescape(value)
 		if err != nil {
-			return nil, err
+			// QueryUnescape returns an error like "invalid escape: '%x'".
+			// But frontends record these errors to prometheus, which generates
+			// a lot of time series.
+			// We log it here for debugging instead.
+			log.WithFields(log.Fields{"error": err}).Debug("failed to unescape query param value")
+			return nil, ErrInvalidQueryEscape
 		}
 
 		if key == "info_hash" {
