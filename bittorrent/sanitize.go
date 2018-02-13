b19f7115dffcbe7fa64d1264b5f34294f00b2613
Leo Balduf <baldufle@hu-berlin.de>
2018-02-13 10:13:57 +0100

    bittorrent: validate port != 0 for announces

--- bittorrent/sanitize.go
+++ bittorrent/sanitize.go
@@ -9,9 +9,16 @@ import (
 // ErrInvalidIP indicates an invalid IP for an Announce.
 var ErrInvalidIP = ClientError("invalid IP")
 
+// ErrInvalidPort indicates an invalid Port for an Announce.
+var ErrInvalidPort = ClientError("invalid port")
+
 // SanitizeAnnounce enforces a max and default NumWant and coerces the peer's
 // IP address into the proper format.
 func SanitizeAnnounce(r *AnnounceRequest, maxNumWant, defaultNumWant uint32) error {
+	if r.Port == 0 {
+		return ErrInvalidPort
+	}
+
 	if !r.NumWantProvided {
 		r.NumWant = defaultNumWant
 	} else if r.NumWant > maxNumWant {
